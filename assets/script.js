8a7a57ede98a6ebb249d00a71921e0574054411a
Talal <talal.i.sharaa@gmail.com>
2024-03-22 13:40:25 +0300

    Refactor nav link active class logic

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,17 @@
 // Get all nav links
-var navLinks = document.querySelectorAll('.navbar a');
+const navLinks = document.querySelectorAll(".navbar a");
+
+// Get current URL path and filename
+const page = window.location.pathname.split("/").pop();
 
 // Loop through links
-for (var i = 0; i < navLinks.length; i++) {
-    // Remove active class from all items
-    navLinks[i].classList.remove('active');
+navLinks.forEach((link) => {
+  // Remove active class from all items
+  link.classList.remove("active");
 
-    // Check if link href matches current page URL
-    if (navLinks[i].getAttribute('href') === '.' + window.location.pathname) {
-        // Add active class
-        navLinks[i].classList.add('active');
-    }
-}
+  // Check if link href matches current page URL
+  if (link.getAttribute("href").split("/").pop() == page) {
+    // Add active class
+    link.classList.add("active");
+  }
+});
